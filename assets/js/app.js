fd658858a82260f6592479efbd013882298475e2
Semenov Daniil <ds.daniilsemen.ds@gmail.com>
2024-06-25 12:06:04 +0300

    Update preloader animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import "./cookie.js"
 import "./preloader.js"
 import "./animateHeader.js"
+import "./scrollWindow.js"
 import "./moveGradientBlick.js";
 import "./calendar.js";
 // import "./model-modile.js"
--- assets/js/scrollWindow.js
+++ assets/js/scrollWindow.js
@@ -1,13 +1,10 @@
 const body = document.body.getBoundingClientRect();
-const header = document.querySelector('header');
 const index_section = [...document.querySelector('.index-main').getElementsByTagName('section')];
 
 window.addEventListener('scroll', function(event) {
-    if (scrollY > 0) {
-        header.classList.remove('header-start');
-        header.classList.remove('header-a');
-        header.classList.add('header-skroll');
-    } else {
-        header.classList.remove('header-skroll');
-    }
+    console.log(this.scrollY, body.height);
+
+    index_section.forEach(section => {
+        console.log(section.getBoundingClientRect());
+    });
 });
